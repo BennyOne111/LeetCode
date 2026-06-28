511bd1cbd9360c3e124624da1a71edd8ed3a8844
BennyOne111 <74817008+BennyOne111@users.noreply.github.com>
2026-06-28 16:47:04 +0800

    LC-word-pattern [Runtime: N/A; Memory: N/A]

---
+++ hash-table/word-pattern.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        words = s.split()
+
+        if len(pattern) != len(words):
+            return false
+
+        char_to_word = {}
+        word_to_char = {}
+
+        for c, word in zip(pattern, words):
+            if c in char_to_word:
+                if char_to_word[c] != word:
+                    return False
+            else:
+                char_to_word[c] = word
+
+            if word in word_to_char:
+                if word_to_char[word] != c:
+                    return False
+            else:
+                word_to_char[word] = c;
+
+        return True 
