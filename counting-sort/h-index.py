f33242f8845c2cd97dcb6280f98c555c48442e2d
BennyOne111 <74817008+BennyOne111@users.noreply.github.com>
2026-06-28 21:31:45 +0800

    LC-h-index [Runtime: N/A; Memory: N/A]

--- counting-sort/h-index.py
+++ counting-sort/h-index.py
@@ -1,12 +1,14 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort()
-        n = len(citations)
+        citations.sort(reverse=True)
+
+        h = 0
 
         for i, c in enumerate(citations):
-            h = n - i
-            if c >= h:
+            if c >= i + 1:
+                h = i + 1
+            else:
                 break
-        
+
         return h
         
