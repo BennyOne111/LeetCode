5356609e9119f9172a6c8a71eea88c456a505c79
BennyOne111 <74817008+BennyOne111@users.noreply.github.com>
2026-06-28 17:28:36 +0800

    LC-search-insert-position [Runtime: N/A; Memory: N/A]

---
+++ binary-search/search-insert-position.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums)
+
+        while left < right:
+            mid = (left + right) // 2;
+            if target <= nums[mid]:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
