1a5530941da8c62ed70bd3f65bb76773c5d826b0
BennyOne111 <74817008+BennyOne111@users.noreply.github.com>
2026-07-09 22:02:46 +0800

    LC-length-of-longest-subarray-with-at-most-k-frequency [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        count = defaultdict(int)
+        max_len = 0
+        left = 0
+
+        for right in range(len(nums)):
+            count[nums[right]] += 1
+            while count[nums[right]] > k:
+                left += 1
+                count[nums[left]] -= 1
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
+        
